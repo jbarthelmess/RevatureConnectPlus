44445f84d1b20efc5e03bf2294e1e15c730066ec
lip <phillip@lipco.dev>
2021-03-16 16:58:44 -0700

    services and controllers are working, confirmed server loads get requests, will confirm with post and patch tomorrow

--- src/main/java/dev/group1/RevatureConnectPlus/RevatureConnectPlusApplication.java
+++ src/main/java/dev/group1/RevatureConnectPlus/RevatureConnectPlusApplication.java
@@ -3,14 +3,17 @@ package dev.group1.RevatureConnectPlus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = {"dev.group1"})
 @EnableJpaRepositories(basePackages = {"dev.group1.repos"})
 @EntityScan(basePackages = {"dev.group1.entities"})
 @SpringBootApplication
 public class RevatureConnectPlusApplication {
 
 	public static void main(String[] args) {
+		// auto creating application based on the info provided...
 		SpringApplication.run(RevatureConnectPlusApplication.class, args);
 	}
 
