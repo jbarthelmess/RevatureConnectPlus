8fecf285790c5196997669566a1952a10ad0dcc1
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-03-17 13:07:35 -0400

    added JWT util

---
+++ src/main/java/dev/group1/utils/JwtUtil.java
@@ -0,0 +1,24 @@
+package dev.group1.utils;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import dev.group1.entities.User;
+
+public class JwtUtil {
+    private static final String secret = System.getenv("JWT");
+    private static final Algorithm algo = Algorithm.HMAC512(secret);
+    public static String generate(User user) {
+        return JWT.create().withClaim("userId",user.getUserId()).withClaim("username", user.getUsername()).withClaim("displayName", user.getDisplayName()).sign(algo);
+    }
+
+    public static DecodedJWT isValidJWT(String token) {
+        try {
+            return JWT.require(algo).withClaimPresence("userId").withClaimPresence("username").withClaimPresence("displayName").build().verify(token);
+        } catch(JWTVerificationException e) {
+            // will add logging later, or in an aspect
+            return null;
+        }
+    }
+}
