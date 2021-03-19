e83984b660212f9b69bff601c5c9e6016546dafa
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-03-19 00:12:22 -0400

    added authorize import to postcontroller

--- src/main/java/dev/group1/controllers/PostController.java
+++ src/main/java/dev/group1/controllers/PostController.java
@@ -6,6 +6,7 @@ import dev.group1.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import dev.group1.aspects.Authorize;
 
 import java.util.Set;
 
